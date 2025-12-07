3f59ab1c68a1e376363e50a698ca1a01a23c7b94
Hariveeraprasad-007 <arikatlahariveeraprasad007@gmail.com>
2025-12-07 11:15:45 +0530

    reversing k element in array

---
+++ arrays/Reverse_k_elements.java
@@ -0,0 +1,29 @@
+package arrays;
+
+public class Reverse_k_elements {
+    public static void reverse_elements(int[] arr,int k){
+        for(int i=0;i<arr.length;i+=k){
+            int left=i;
+            int right=Math.min(i+k-1,arr.length-1);
+            while(left<=right){
+                int temp=arr[left];
+                arr[left]=arr[right];
+                arr[right]=temp;
+                left++;
+                right--;
+            }
+        }
+    }
+    public static void print_array(int[] arr){
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5,6,7,8};
+        print_array(arr);
+        reverse_elements(arr,3);
+        print_array(arr);
+    }
+}
